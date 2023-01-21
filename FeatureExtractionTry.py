e5f2f9960d7bebb06b4bd6f058d3f427a1292f50
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-01-21 14:38:28 +0700

    blood vessel in imageprocessing3

--- FeatureExtractionTry.py
+++ FeatureExtractionTry.py
@@ -10,7 +10,7 @@ from skimage.feature import graycomatrix, graycoprops
 import skimage.feature as feature
 import os
 
-path_dataset = "E:/Pawang Code/Diabetic Retinopathy/dataset"
+path_dataset = "E:/Pawang Code/Diabetic Retinopathy/dataset_filter"
 book = xls.Workbook('E:/Pawang Code/Diabetic Retinopathy/featureExtraction3.xlsx')
 sheet = book.add_worksheet()
 sheet.write(0, 0, 'Image')
@@ -25,19 +25,60 @@ sheet.write(0,7, 'Label')
 column += 1
 row = 1
 
-def preprocessing(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, mg = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY_INV)
-    reverse = cv2.bitwise_not(mg)
-    contours, hierarchy = cv2.findContours(reverse, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    select = max(contours, key=cv2.contourArea)
-    x, y, w, h = cv2.boundingRect(select)
-    png = img[y:y+h, x:x+w]
-    png = cv2.resize(png, (500, 500))
-    green = png[:, :, 1]
-    # clahe = exposure.equalize_adapthist(green, clip_limit=0.03)
+def crop_image_from_gray(img,tol=7):
+    if img.ndim ==2:
+        mask = img>tol
+        return img[np.ix_(mask.any(1),mask.any(0))]
+    elif img.ndim==3:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        mask = gray_img>tol        
+        check_shape = img[:,:,0][np.ix_(mask.any(1),mask.any(0))].shape[0]
+        if (check_shape == 0):
+            return img
+        else:
+            img1=img[:,:,0][np.ix_(mask.any(1),mask.any(0))]
+            img2=img[:,:,1][np.ix_(mask.any(1),mask.any(0))]
+            img3=img[:,:,2][np.ix_(mask.any(1),mask.any(0))]
+            img = np.stack([img1,img2,img3],axis=-1)
+            return img
 
-    return green
+def circle_crop_v2(img):
+    img = crop_image_from_gray(img)
+
+    height, width, depth = img.shape
+    largest_side = np.max((height, width))
+    img = cv2.resize(img, (largest_side, largest_side))
+
+    height, width, depth = img.shape
+
+    x = int(width / 2)
+    y = int(height / 2)
+    r = np.amin((x, y))
+
+    circle_img = np.zeros((height, width), np.uint8)
+    cv2.circle(circle_img, (x, y), int(r), 1, thickness=-1)
+    img = cv2.bitwise_and(img, img, mask=circle_img)
+    img = crop_image_from_gray(img)
+
+    return img
+
+def preprocessing(img):
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # ret, mg = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY_INV)
+    # reverse = cv2.bitwise_not(mg)
+    # contours, hierarchy = cv2.findContours(reverse, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # select = max(contours, key=cv2.contourArea)
+    # x, y, w, h = cv2.boundingRect(select)
+    # png = img[y:y+h, x:x+w]
+    # png = cv2.resize(png, (500, 500))
+    # green = png[:, :, 1]
+    # clahe = exposure.equalize_adapthist(green, clip_limit=0.03)
+    green = img[:,:,1]
+    # incomplement = cv2.bitwise_not(green) # negative image
+    clache = cv2.createCLAHE(clipLimit=5) # Contrast Limited Adaptive Histogram Equalization
+    cl1 = clache.apply(green) # Apply CLAHE
+
+    return cl1
 
 def glcm(img):
     distance = [5]
@@ -61,6 +102,7 @@ for folder in os.listdir(path_dataset):
         column += 1
 
         img = cv2.imread(os.path.join(path_dataset, folder, filename))
+        img = circle_crop_v2(img)
         preprocessed = preprocessing(img)
 
         glcm_props = glcm(preprocessed)
